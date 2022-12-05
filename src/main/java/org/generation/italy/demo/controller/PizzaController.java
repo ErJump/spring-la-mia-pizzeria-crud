36ac42c9b8fd02bfeb0bd7abc9922562e37b57dd
ErJump <fedelijunior@gmail.com>
2022-12-05 15:54:46 +0100

    feat: :sparkles: Adds crate and store methods + form

--- src/main/java/org/generation/italy/demo/controller/PizzaController.java
+++ src/main/java/org/generation/italy/demo/controller/PizzaController.java
@@ -8,7 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -23,5 +27,21 @@ public class PizzaController {
 		List<Pizza> pizzas = pizzaService.findAll();
 		model.addAttribute("pizzas", pizzas);
 		return "index";
+	}
+	
+	@GetMapping("/pizza/create")
+	public String createPizza(Model model) {
+		
+		Pizza pizza = new Pizza();
+		model.addAttribute("pizza", pizza);
+		
+		return "pizza-create";
+	}
+	
+	@PostMapping("/pizza/create")
+	public String storePizza(@Valid @ModelAttribute("pizza") Pizza pizza) {
+		
+		pizzaService.save(pizza);
+		return "redirect:/";
 	}
 }
