e452caf8fa970b449d9e631b3c6991d42d841ad4
ErJump <fedelijunior@gmail.com>
2022-12-05 15:12:24 +0100

    feat: :sparkles: Adds index method

--- src/main/java/org/generation/italy/demo/controller/PizzaController.java
+++ src/main/java/org/generation/italy/demo/controller/PizzaController.java
@@ -1,5 +1,27 @@
 package org.generation.italy.demo.controller;
 
-public class PizzaController {
+import java.util.List;
 
+import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PizzaController {
+	
+	@Autowired
+	private PizzaService pizzaService;
+	
+	@GetMapping
+	public String index(Model model) {
+		
+		List<Pizza> pizzas = pizzaService.findAll();
+		model.addAttribute(pizzas);
+		return "index";
+	}
 }
