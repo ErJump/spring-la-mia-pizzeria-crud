a564d01c5f3eae2b367ce0c997f0eaf3de6e0ad1
ErJump <fedelijunior@gmail.com>
2022-12-06 12:38:43 +0100

    feat: :test_tube: Adds some drinks for testing

--- src/main/java/org/generation/italy/demo/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/generation/italy/demo/SpringLaMiaPizzeriaCrudApplication.java
@@ -1,6 +1,8 @@
 package org.generation.italy.demo;
 
+import org.generation.italy.demo.pojo.Drink;
 import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.service.DrinkService;
 import org.generation.italy.demo.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,6 +14,9 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PizzaService pizzaService;
+	
+	@Autowired
+	private DrinkService drinkService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
@@ -27,6 +32,14 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
 		pizzaService.save(p1);
 		pizzaService.save(p2);
 		pizzaService.save(p3);
+		
+		Drink d1 = new Drink("Coca Cola", "la classica, la migliore" , 2);
+		Drink d2 = new Drink("Fanta", "Aranciata, arancione, gialla? Che buona!" , 1);
+		Drink d3 = new Drink("Sprite", "Frizzantina, una punta di limone, la gassosa!" , 5);
+		
+		drinkService.save(d1);
+		drinkService.save(d2);
+		drinkService.save(d3);
 	}
 
 }
